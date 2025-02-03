3339e5a5951e0cdb7687fb562e24513f21f572be
Polaris724 <lachy722@gmail.com>
2025-02-03 16:25:24 +1100

    small changes before ending day

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ def main():
         text = f.read()
 
     #print(text)
-
-    word_count = len(text)
+    
+    word_count = len(text.split())
     print(word_count)
 
     characters = {}
     text = text.lower()
-    
+
     for char in text:
         if char in characters:
             characters[char] += 1
         else:
             characters.update({char:1})
-    print(characters)
+     #print(characters)
+
+    
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document/n")
+
 
 main()
