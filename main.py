75ca0478e7bbb05138b1995fa69d9b77bfe287f0
Polaris724 <lachy722@gmail.com>
2025-02-03 15:40:43 +1100

    Completed word counter and character counter

---
+++ main.py
@@ -0,0 +1,20 @@
+def main():
+    with open("books/frankenstein.txt") as f:
+        text = f.read()
+
+    #print(text)
+
+    word_count = len(text)
+    print(word_count)
+
+    characters = {}
+    text = text.lower()
+    
+    for char in text:
+        if char in characters:
+            characters[char] += 1
+        else:
+            characters.update({char:1})
+    print(characters)
+
+main()
