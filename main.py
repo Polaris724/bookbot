e157916f70c1f352bced58a7fdc6379f9d5f7311
Polaris724 <lachy722@gmail.com>
2025-02-05 12:08:11 +1100

    Completed BookBot

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+def sort_on(dict):
+        return dict["num"]
+
 def main():
     with open("books/frankenstein.txt") as f:
         text = f.read()
 
-
-    #print(text)
+    #print(text) --> If wanting to output the .txt file
     
     word_count = len(text.split())
-    print(word_count)
 
     characters = {}
     text = text.lower()
@@ -16,17 +17,19 @@ def main():
             characters[char] += 1
         else:
             characters.update({char:1})
-     #print(characters)
+     #print(characters) -> Debugging for loop
 
     print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{word_count} words found in the document/n")
-    
-    for char in characters:
+    print(f"{word_count} words found in the document")
+    print()
+    char_list = []
+    for char, count in characters.items():
         if char.isalpha():
+            char_list.append({'name': char, 'num': count})
             
-
-    for
-        print(f"The '{}' character was found {} times")
-
+    char_list.sort(reverse=True, key=sort_on)
+    for char_dict in char_list:
+       print(f"The '{char_dict['name']}' character was found {char_dict['num']} times")
+    print("--- End report ---")
 
 main()
